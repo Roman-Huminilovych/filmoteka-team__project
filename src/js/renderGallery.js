29944f6e5c8f16769a166ce377d3b8c00c784fe7
Roman-Huminilovych <103421237+Roman-Huminilovych@users.noreply.github.com>
2022-11-06 11:57:59 +0200

    Пофіксив баги

--- src/js/renderGallery.js
+++ src/js/renderGallery.js
@@ -1,12 +1,9 @@
 import cardTemplate from '../templates/cardTemplate.hbs';
 
-
 const refs = {
-  gallery: document.querySelector('.gallery'),
+  gallery: document.querySelector('.films'),
 };
-export function  createMarkup(objects) {
+export function createMarkup(objects) {
   const markup = cardTemplate(objects);
   refs.gallery.insertAdjacentHTML('beforeend', markup);
 }
-
-
