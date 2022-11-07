62ad82ae86b9b9ee11a846930ea78e0515db7a77
Alina-Halchenko <104688698+Alina-Halchenko@users.noreply.github.com>
2022-11-07 03:05:07 +0200

    draft of the slider

--- src/index.js
+++ src/index.js
@@ -42,3 +42,21 @@ createMarkup([
 
 activatePagination({current: 15, pages: 50});
 
+
+
+// import $ from 'jquery';
+// import 'slick-carousel';
+
+
+$('.carousel').slick({
+  dots: true,
+  arrows: false,
+  speed: 1000,
+  easing: 'ease',
+  autoplay: true,
+  autoplaySpeed: 5000,
+  pauseOnFocus: true,
+  pauseOnHover: true,
+  pauseOnDotsHover: true,
+});
+
