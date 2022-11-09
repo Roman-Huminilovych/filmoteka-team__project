b15816ee1a7b4ec2fa31ca76d6784a04cc3ab4bf
Daria-Zakharova <dariazakharova.smthelse@gmail.com>
2022-11-10 00:07:51 +0200

    add close button, fix genres

--- src/js/modalOpenClose.js
+++ src/js/modalOpenClose.js
@@ -3,29 +3,34 @@ import { getMovieById } from './get-movie-info';
 import modalFilm from '../templates/modalFilm.hbs';
 
 const backdrop = document.querySelector('.backdrop');
-const modalBtn = document.querySelector('.modal-close-btn');
+
 const films = document.querySelector('.films');
 
 films.addEventListener('click', onOpenModal);
 export function onOpenModal(evt) {
   evt.preventDefault();
-  console.log(evt);
+  // console.log(evt);
   const currentItem = evt.target.closest('li');
-  console.log(currentItem);
+  // console.log(currentItem);
   let id = +currentItem.dataset.id;
   async function onMovieClick() {
     try {
       const movieInfo = await (await getMovieById(id)).data;
-      console.log('movieInfo', movieInfo);
-      backdrop.innerHTML = modalFilm(movieInfo.results);
       renderModalFilm({ movieInfo });
     } catch (error) {
       console.log(error.message);
     }
   }
-  onMovieClick();
+
+  onMovieClick().then(() => {
+    const modalBtn = document.querySelector('.modal .modal-close-btn');
+
+    modalBtn.addEventListener('click', closeModal);
+
+  });
 
   backdrop.classList.remove('is-hidden');
+  
   const instance = basicLightbox.create(backdrop, {
     onShow: () => {
       document.addEventListener('keydown', onCloseModalEsc);
@@ -38,26 +43,25 @@ export function onOpenModal(evt) {
 
   function onCloseModalEsc(evt) {
     if (evt.code === 'Escape') {
-      instance.close();
-      backdrop.classList.add('is-hidden');
+      closeModal();
     }
-  }
-
-  modalBtn.addEventListener('click', onCloseModalBtn);
-  function onCloseModalBtn(evt) {
-    instance.close();
-    backdrop.classList.add('is-hidden');
   }
 
   backdrop.addEventListener('click', onCloseModalBack);
   function onCloseModalBack(evt) {
     if (evt.target === evt.currentTarget) {
-      instance.close();
-      backdrop.classList.add('is-hidden');
+      closeModal();
     }
+  }
+
+  function closeModal() {
+    instance.close();
+    backdrop.classList.add('is-hidden');
   }
 }
 
-export function renderModalFilm(film) {
-  backdrop.insertAdjacentHTML('beforeend', modalFilm(film));
-}
+function renderModalFilm(film) {
+  backdrop.innerHTML = modalFilm(film);
+} 
+
+  
