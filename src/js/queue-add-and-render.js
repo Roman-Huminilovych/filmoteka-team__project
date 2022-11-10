81e0f6b775a436db55f8d1b85b12791b265f2ee5
Andrew Burlachenko <ap.burla4enko@gmail.com>
2022-11-10 20:28:51 +0300

    Update queue-add-and-render.js

--- src/js/queue-add-and-render.js
+++ src/js/queue-add-and-render.js
@@ -1,49 +1,52 @@
 //F15, F19
 import Notiflix from 'notiflix';
-import createMarkup from './render-searchQuery';
+import { createMarkup } from './render-searchQuery';
 
 const container = document.querySelector('.films');
 
 //кнопка добавления фильма в список (на модалке)
-const addToQueueBtn = document.querySelector('button.js-addQueueBtn'); 
+const addToQueueBtn = document.querySelector('button.js-addQueueBtn');
 
 //кнопка "QUEUE" на странице библиотеки
-const renderQueueBtn = document.querySelector('button#queue-btn'); 
+const renderQueueBtn = document.querySelector('button#queue-btn');
 
 //список фильмов для рендера странички
-const queueList = JSON.parse(localStorage.getItem("queueList")) || [];
+const queueList = JSON.parse(localStorage.getItem('queueList')) || [];
 
-
-//функция добавления фильма в очередь 
+//функция добавления фильма в очередь
 addToQueueBtn.addEventListener('click', addMovieToQueue);
-export function addMovieToQueue (object) {
-    //console.log(object);  //проверить, что приходит
-        
-   // const newMovie = {...object};  
-    const newMovie = ({poster_path, title, genre_ids, release_date, vote_average} = object);
-    //console.log(newMovie); // проверить, что создано
+export function addMovieToQueue(object) {
+  //console.log(object);  //проверить, что приходит
 
-    queueList.push(newMovie);
-    localStorage.setItem('queueList', JSON.stringify(queueList));
+  // const newMovie = {...object};
+  const newMovie = ({
+    poster_path,
+    title,
+    genre_ids,
+    release_date,
+    vote_average,
+  } = object);
+  //console.log(newMovie); // проверить, что создано
+
+  queueList.push(newMovie);
+  localStorage.setItem('queueList', JSON.stringify(queueList));
 }
 
 renderQueueBtn.addEventListener('click', queueMarkup);
-export function queueMarkup (evt) {
-    evt.preventDefault();
-    container.innerHTML = '';
+export function queueMarkup(evt) {
+  evt.preventDefault();
+  container.innerHTML = '';
 
-    //ЗДЕСЬ МОЖНО ВСТАВИТЬ ФУНКЦИЮ СПИННЕРА, ПОКА РЕНДЕРИТСЯ РАЗМЕТКА
+  //ЗДЕСЬ МОЖНО ВСТАВИТЬ ФУНКЦИЮ СПИННЕРА, ПОКА РЕНДЕРИТСЯ РАЗМЕТКА
 
-    const queueList = JSON.parse(localStorage.getItem("queueList"));
-    if(!queueList) {
-        Notiflix.Notify.info('Your queue is empty. You can add movies to queue on main page.');
+  const queueList = JSON.parse(localStorage.getItem('queueList'));
+  if (!queueList) {
+    Notiflix.Notify.info(
+      'Your queue is empty. You can add movies to queue on main page.'
+    );
 
-        return;
-    }
-    
-    return createMarkup(queueList);
+    return;
+  }
+
+  return createMarkup(queueList);
 }
-
-
-
-
