c38502f8688584da0397ef18d8b1e138fd3088a5
Daria-Zakharova <dariazakharova.smthelse@gmail.com>
2022-11-12 21:02:44 +0200

    replace then, refactor functions

--- src/js/modalOpenClose.js
+++ src/js/modalOpenClose.js
@@ -1,10 +1,6 @@
 import { getMovieById } from './get-movie-info';
 import modalFilm from '../templates/modalFilm.hbs';
-import { addToQueueOnClick } from './queue-add';
-import { addToWatchedOnClick } from './watched-add';
-import { deleteFromQueue } from './secondary-functions/delete-from-queue';
-
-const backdrop = document.querySelector('.backdrop__movie-info');
+import { activateModalBtns } from './secondary-functions/activate-movie-modal';
 
 export async function onOpenModal(evt) {
   evt.preventDefault();
@@ -17,76 +13,17 @@ export async function onOpenModal(evt) {
 
       const movieInfo = await (await getMovieById(id)).data;
       renderModalFilm({ movieInfo });
-      backdrop.classList.remove('is-hidden');    
+      activateModalBtns();   
     } 
     catch (error) {
       console.log(error.message);
     }
   }
-
-  // добавление слушателей после создания модального окна
-  createModal().then(() => {
-    const modalCloseBtn = document.querySelector('.modal .modal-close-btn');
-    const addToQueueBtn = document.querySelector('.modal__btn--queue');
-    const addToWatchedBtn = document.querySelector('.modal__btn--watched');
-    const removeBtn = document.querySelector('.modal__btn--queue-remove');
-    
-    document.addEventListener('keydown', onCloseModalEsc);
-    // закрывает модальное окно по клику вне модального окна
-    backdrop.addEventListener('click', onCloseModalBack); 
-    modalCloseBtn.addEventListener('click', closeModal);
-
-    // для страницы "просмотренные"
-    if (document.querySelector('#watched-btn.active')) {
-      const buttonList = document.querySelector('.library-cont');
-      buttonList.style.display = 'none';
-    } 
-    // для страницы очереди
-    else if (document.querySelector('#queue-btn.active')) {
-      addToQueueBtn.style.display = 'none';
-
-      removeBtn.addEventListener('click', deleteFromQueue);
-      removeBtn.addEventListener('click', closeModal);
-      addToWatchedBtn.addEventListener('click', addToWatchedOnClick);
-      addToWatchedBtn.addEventListener('click', closeModal);
-    } 
-    // для домашней страницы
-    else {
-      removeBtn.style.display = 'none';
-
-      addToQueueBtn.addEventListener('click', addToQueueOnClick);
-      addToWatchedBtn.addEventListener('click', addToWatchedOnClick);
-    }
-  })
-  .catch((error) => console.log(error.message));
+  createModal();
 }
-
-function onCloseModalEsc(evt) {
-    if (evt.code === 'Escape') {
-      closeModal();
-    }
-  }
-
-function onCloseModalBack(evt) {
-    if (evt.target === evt.currentTarget) {
-      closeModal();
-    }
-  }
-
-function closeModal() {
-  const addToQueueBtn = document.querySelector('.modal__btn--queue');
-  addToQueueBtn.removeEventListener('click', addToQueueOnClick);
-  const addToWatchedBtn = document.querySelector('.modal__btn--watched');
-  addToWatchedBtn.removeEventListener('click', addToWatchedOnClick);
-
-  backdrop.classList.add('is-hidden');
-  document.querySelector('body').classList.remove('modal-open');
-  document.removeEventListener('keydown', onCloseModalEsc);
-}
-
-
 
 function renderModalFilm(film) {
+  const backdrop = document.querySelector('.backdrop__movie-info');
   backdrop.innerHTML = modalFilm(film);
   const votes = document.querySelector('.modal__attributes-vote');
   const popularity = document.querySelector('.modal__attributes-text--popular');
@@ -98,4 +35,5 @@ function renderModalFilm(film) {
     popularity.textContent % 1 === 0
       ? popularity.textContent
       : (+popularity.textContent).toFixed(1);
+  backdrop.classList.remove('is-hidden'); 
 }
